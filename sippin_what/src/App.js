e4cba44cb0ff1ff6f12915377efb47645e1e589a
Nathaniel Jansen <nathanieljansen@gmail.com>
2018-07-12 19:39:21 -0600

    node_modules be gone

--- sippin_what/src/App.js
+++ sippin_what/src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Homepage from "./components/Homepage";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Sippin What</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Homepage></Homepage>
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <h1 className="App-title">Sippin What</h1>
+    //     </header>
+    //     <p className="App-intro">
+    //       To get started, edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //   </div>
     );
-  }
+    }
 }
 
 export default App;
