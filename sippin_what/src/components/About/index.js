a115c886d545ab48406c00c84feb8d458217a47a
Nathaniel Jansen <nathanieljansen@gmail.com>
2018-07-19 20:15:12 -0600

    fixes

--- sippin_what/src/components/About/index.js
+++ sippin_what/src/components/About/index.js
@@ -5,24 +5,18 @@ import carol from "./images/carol.jpg";
 import david from "./images/david.jpg";
 import pritha from "./images/pritha.jpg";
 import nate from "./images/nate.jpg";
-import bottles from "./images/bottles.jpg";
-// import Parallax from '../src/Parallax';
 
 //super means start up the library
 
 
-    class About extends React.Component {
-        constructor(props) {
-            super(props)
-        }
-        render() {
-            return(
-                    <div>
-    <Row>
-        <Col s={12}><h3>The Sippin What Team</h3></Col>
-    </Row>
+class About extends React.Component {
+render() {
+    return(
     <div>
-
+        <Row>
+            <Col s={12}><h3>The Sippin What Team</h3></Col>
+        </Row>
+     <div>
     <Row>
         <Col s={2}></Col>
         <Col s={3}>
@@ -67,59 +61,6 @@ import bottles from "./images/bottles.jpg";
         }
     }
 
-
 export default About;
 
-// const About = props => ( 
-//   <div>
-
-
-//     <Row>
-//         <Col s={12}><h3>The Sippin What Team</h3></Col>
-//     </Row>
-//     <div>
-
-//     <Row>
-//         <Col s={2}></Col>
-//         <Col s={3}>
-//             <Card id="card" header={<CardTitle image={carol} waves='light'/>}
-//             title="Carol"
-//             reveal={<p>Carol is a driven professional who has spent most of her career in the non-profit world. She has a demonstrated history of building strong relationships with staff, clients and donors, managing projects and developing social media and marketing strategies. When not working she can be found enjoying the outdoors, coding, and wine (of course).</p>}>
-//             <p><a href="https://www.linkedin.com/in/carol-g-0a9b15b4/">Connect with Carol</a></p>
-//         </Card>
-//         </Col>
-//         <Col s={2}></Col>
-//         <Col s={3}>
-//             <Card id="card" header={<CardTitle image={david} waves='light'/>}
-//             title="David"
-//             reveal={<p>David is an experienced Technical Account Manager and Technical Support Engineer in the television broadcast industry. Currently studying HTML5, Javascript, JQuery, and Node, at the University of Denver, David is gaining knowledge of fullstack web development, including interacting with APIs, JSON messaging and backend database querying.What? It's only been two months?</p>}>
-//             <p><a href="https://www.linkedin.com/in/davem/">Connect with David</a></p>
-//         </Card>
-//         </Col>
-//         <Col s={2}></Col>
-//     </Row>    
-//     < Row>
-//         <Col s={2}></Col>
-//        <Col s={3}>
-//             <Card id="card" header={<CardTitle image={pritha} waves='light'/>}
-//             title="Pritha"
-//             reveal={<p>Pritha is an experienced Software Test Engineer with a demonstrated history of working in the finance and health wellness and fitness industry. She is a quality assurance professional and a graduate from Columbia College. Besides work, she is learning how to ski and love to travel to new places to explore different culture and try new food.</p>}>
-//             <p><a href="https://www.linkedin.com/in/pritha-mishra/">Connect with Pritha</a></p>
-//         </Card>
-//         </Col>
-//            <Col s={2}></Col>
-//         <Col s={3}>
-//             <Card id="card" header={<CardTitle image={nate} waves='light'/>}
-//             title="Nate"
-//             reveal={<p>Nathaniel has a strong background in process management and development. His past knowledge in technology and business managment has driven him to excel in his field.Nathaniel loves writing code and is motivated to develop web applications that are bug free and sophisticated.Besides his passion about writing code Nate enjoys the occasional drink.So, Sip This to support him and the rest of the team - Cheers!</p>}>
-//             <p><a href="https://www.linkedin.com/in/nathanieljansen/">Connect with Nate</a></p>
-//         </Card>
-//         </Col> 
-//          <Col s={2}></Col>
-//     </Row>
-//     </div>
-//   </div>
-// );
-
-// export default About;
 
--- sippin_what/src/components/ContactForm/ContactFormContainer.js
+++ sippin_what/src/components/ContactForm/ContactFormContainer.js
@@ -17,28 +17,27 @@ class ContactFormContainer extends Component {
             password: '',
             is21: false,
             shouldRenderContactForm: true,
-            // shouldRenderLoginForm: false,
-            // shouldRenderSignupForm: false,
+
         }
     }
 
-    handleInputChange = event => {
+handleInputChange = event => {
         const value = event.target.value;
         const name = event.target.name;
         this.setState({
             [name]: value
         }, () => console.log(this.state));
-    }
+}
 
-    handleFormSubmit = event => {
-        event.preventDefault();
+handleFormSubmit = event => {
+    event.preventDefault();
 
-        // console.log("STATE" + this.state);
 
-        const {
-            name,
-            email,
-            message
+
+    const {
+        name,
+        email,
+        message
         } = this.state;
 
         const user = {
@@ -46,11 +45,7 @@ class ContactFormContainer extends Component {
             email: email,
             message: message
         };
-        // const user = {
-        //   name: this.state.name,
-        //   email: this.state.email,
-        //   message: this.state.message
-        // };
+
 
         console.log(name, email, message + " was submitted ")
         console.log(message, email, name);
@@ -84,16 +79,6 @@ class ContactFormContainer extends Component {
            
 
             </div>
-
-            //   <div className="App">
-            //     <header className="App-header">
-            //       <img src={logo} className="App-logo" alt="logo" />
-            //       <h1 className="App-title">Sippin What</h1>
-            //     </header>
-            //     <p className="App-intro">
-            //       To get started, edit <code>src/App.js</code> and save to reload.
-            //     </p>
-            //   </div>
         );
     }
 }
