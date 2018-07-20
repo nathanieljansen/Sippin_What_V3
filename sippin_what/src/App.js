7247db252b5168fb964ef4a6b098d35dda9dc2fc
Nathaniel Jansen <nathanieljansen@gmail.com>
2018-07-19 19:44:07 -0600

    merge fixes

--- sippin_what/src/App.js
+++ sippin_what/src/App.js
@@ -1,45 +1,23 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import HeaderContainer from "./components/Header/HeaderContainer"
 import HomepageContainer from "./components/Homepage/HomepageContainer";
 import FooterContainer from "./components/Footer/FooterContainer"
 import About from "./components/About";
-
-class App extends Component {   
-// import logo from './logo.svg';
-
-// import Homepage from "./components/Homepage";
-=======
-
-
-
 import ContactFormContainer from "./components/ContactForm/ContactFormContainer"
-//
-class App extends Component {
-  
-
->>>>>>> better-vino
-
+class App extends Component {   
 
   render() {
  
 
     return (
-<<<<<<< HEAD
       <div>
         <HeaderContainer />
         <HomepageContainer />
         <FooterContainer />
         <About />
+        < ContactFormContainer / >
       </div>
     )
-=======
-      <ContactFormContainer />
-    );
-
-      
-      
->>>>>>> better-vino
   }
 }
 
