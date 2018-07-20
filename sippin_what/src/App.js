1806a53e185463b267525b57d007d5ed41eac56d
Carol Giblin <giblinCK@gmail.com>
2018-07-19 21:41:35 -0600

    route troubles

--- sippin_what/src/App.js
+++ sippin_what/src/App.js
@@ -1,24 +1,75 @@
 import React, { Component } from 'react';
-import HeaderContainer from "./components/Header/HeaderContainer"
+import HeaderContainer from "./components/Header/HeaderContainer";
 import HomepageContainer from "./components/Homepage/HomepageContainer";
-import FooterContainer from "./components/Footer/FooterContainer"
+import FooterContainer from "./components/Footer/FooterContainer";
 import About from "./components/About";
-import ContactFormContainer from "./components/ContactForm/ContactFormContainer"
+// import Homepage from '.components/Homepage';
+// import ContactForm from './components/ContactForm';
+import ContactFormContainer from "./components/ContactForm/ContactFormContainer";
+import { BrowserRouter as Switch, Route } from "react-router-dom";
 class App extends Component {   
 
   render() {
- 
-
     return (
-      <div>
-        <HeaderContainer />
-        <HomepageContainer />
-        <FooterContainer />
-        <About />
-        <ContactFormContainer />
-      </div>
+     
+     
+        <Switch>
+          <div>
+            < Route path="/home" component={HomepageContainer} />
+            < Route path="/about" component={About} />
+            < Route path="/contact" component={ContactFormContainer} />
+            </div>
+        </Switch>
+        
+     
     )
   }
 }
 
 export default App;
+
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import HomePage from './pages/home.js';
+// import Aboutpage from './pages/about.js';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <Router>
+//         <div>
+//           <Route path="/home" component={HomePage} />
+//           <Route path="/about" component={Aboutpage} />
+//         </div>
+//       </Router>
+//     );
+//   }
+// }
+
+// export default App;
+
+//  <HeaderContainer />
+//             <HomepageContainer />
+//             <FooterContainer />
+//             <About />
+//             <ContactFormContainer />
+
+// import React, { Component } from 'react';
+// // import logo from './logo.svg';
+// import './App.css';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import HomePage from './home.js';
+// // import images from "./images"
+// import About from "./components/about"
+
+// class App extends Component {
+//   render() {
+//     return (
+//      <About></About>
+//     );
+//   }
+// }
+
+// export default App;
