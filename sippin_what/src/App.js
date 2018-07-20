33465afff629cf2b7171c64233bfdf178bfc4dbc
David <dmadacsi1@gmail.com>
2018-07-19 19:09:40 -0600

    updated

--- sippin_what/src/App.js
+++ sippin_what/src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
+
+
 
 import ContactFormContainer from "./components/ContactForm/ContactFormContainer"
 //
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    // 
-  }
+  
 
 
 
   render() {
-    // const {
-    //   shouldRenderContactForm,
-    // } = this.state;
+ 
 
     return (
       <ContactFormContainer />
