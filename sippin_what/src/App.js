1e9cd472e814c55978c7b19fde1f94d29f744656
Carol Giblin <giblinCK@gmail.com>
2018-07-19 18:57:08 -0600

    everything is about to break and database and about

--- sippin_what/src/App.js
+++ sippin_what/src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-
-import Homepage from "./components/Homepage";
+import './App.css';
+// import Homepage from "./components/Homepage";
+import About from "./components/About";
 
 class App extends Component {
   render() {
     return (
-      <Homepage></Homepage>
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <h1 className="App-title">Sippin What</h1>
-    //     </header>
-    //     <p className="App-intro">
-    //       To get started, edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //   </div>
+      // <Homepage></Homepage>
+      <About></About>
+
+    
     );
     }
 }
