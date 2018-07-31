6363313b8d13ff9cf08b9eee81b911db0c738118
Nathaniel Jansen <nathanieljansen@gmail.com>
2018-07-31 06:21:09 -0600

    changes

--- sippin_what/src/components/Header/Header.js
+++ sippin_what/src/components/Header/Header.js
@@ -3,7 +3,7 @@ import {Navbar, NavItem} from 'react-materialize'
 import "./Header.css";
 
 
-const Header = props => (
+const Header = () => (
  
 <div>
 <Navbar className="navBar" brand='Sippin What' href='/'right>
